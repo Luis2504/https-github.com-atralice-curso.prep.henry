39878ad24458438c060f5ec3e195e7b4bb03a267
Luis2504 <luisoctaviojojoalosada@gmail.com>
2021-11-27 18:58:39 -0500

    homework 04

--- 02-JS-I/homework/prueba 1.js
+++ 02-JS-I/homework/prueba 1.js
@@ -175,6 +175,11 @@ if (edad>21){
 
 // imprimir los números del 1 al 100
 
+/* #########################################
+######           A R R A Y S          ######
+############################################ */
+
+
 for (var i=1; i<=100; i++){
     console.log(i)
 }
@@ -290,3 +295,316 @@ function matriz(n,m){
 }
 
 matriz(2,3)
+
+/* ################################################
+#######        C   L  A  S  E  S             ######
+################################################### */
+
+
+/* Las clases son plantillas para construir a partir de objetos; las palntillas son prototipos que definen caractristicas
+genrales de los objetos*/
+
+
+/*Objeto literal 
+####################### */
+var voris = { nombre: 'voris',
+    edad: 23,
+}
+
+var miguel ={
+    nombre:"migue", 
+    edad: 34
+}
+
+/*  Creamos la plantilla para construir el obgeto  para no tener que repetir el codigo
+
+definimos clases(plantila) e imstancias (objetos)*/
+
+/* Functions Constructors  y new 
+###############################
+
+las funsction conctroctors son funciones especiales que construyen objetos */
+
+
+
+function Persona() {
+    this.firstname='Luis';  /* kidwords this */
+    this.lastnmae='Jojoa';
+
+}
+
+var juan = new Persona(); /* new instancia un un objeto o crea un nuevo objeto */
+
+
+
+function Car(marca, color) {
+    this.marca=marca;  /* kidwords this */
+    this.color=color;
+
+}
+
+var Ferrari = new Car('deportivo','black'); /* new instancia un un objeto o crea un nuevo objeto */
+var SixWill = new Car('Pickup', 'Black');
+var Audi = new Car ('Nissan', 'Green')
+
+ /* el keyword new crea un objeto nuevo bacio y glindea el objetos; es decir le da los atributos al
+ objeto de la funcion constructora */
+
+ /* funcion constructora con operadores lógicos
+ ############################################## */
+
+
+ /* con el operador OR 
+ #####################*/
+
+
+
+function Car(marca, color) {
+    this.marca=marca || 'bosbaguen';  /* logigo operado default OR || */
+    this.color=color || 'white';
+
+}
+
+var Ferrari = new Car(); /* new instancia un un objeto o crea un nuevo objeto */
+var SixWill = new Car();
+var Audi = new Car ()
+
+a || b
+
+// tabla de la verdad
+
+T || T = T;
+T || F = T;
+F || T = T;
+F || F = F;  /*  si a es true se marca el primer  valor "a", si a es false se marca el segundo operador "b" */
+
+/* con el operador Y And 
+############################ */
+
+
+function Car(marca, color) {
+    this.marca='bosbaguen' && marca;  /* logigo operado default AND && */
+    this.color= 'white' && color ;
+
+}
+
+var Ferrari = new Car('deportivo', 'green'); /* new instancia un un objeto o crea un nuevo objeto */
+var SixWill = new Car('clásico', 'gris');
+var Audi = new Car ('moderno', 'black');
+
+/* Tabla de la verdad del and && 
+################################*/
+
+
+
+
+T && T = T;
+T && F = F;
+F && T = F;
+F && F = F;  /*  si a es true se marca el segundo valor  "a", si a es false se marca el primer valor "b" */
+
+
+
+/* Prototipes
+###################################### */
+
+/* los cada obgetos que creemos ademas de las propiedades tieene un prototipo; si el obgeto que se crea no tiene una propiedad 
+la trata de asignar desde el prototivo; si no esat definido su resultado es undefined  elprototipo es el la funtions constructor*/
+
+
+function Car(marca, color) {
+    this.marca=marca;  /* prototipo*/
+    this.color=color ;
+
+}
+
+Car.prototype.getmarca=function (){
+    return this.marca;
+}
+                                  /* Define la propiedad que se le asigna */
+
+Car.prototype.getcolor=function (){
+    return this.color;
+}
+
+var Ferrari = new Car('deportivo', 'green'); /* asignacion de la propiedad */
+var SixWill = new Car('clásico', 'gris');
+var Audi = new Car ('moderno', 'black');
+
+
+
+/* ################################################
+#######     T I P O S   D E  C L A S E S     ######
+################################################### */
+
+
+/* CLASES DECLARATORIAS METODO DEL CONSTRUCTOR Y PROTOTIPO 
+#########################################################*/
+
+/* La clase declaratoria crea uan especie de plantilla de varibles declaradas in definidas utilizando la el prototipo
+constructor, que seran llamdas mediante un metodo, (funcion); asi son dinamicas y son llamdas al 
+final siguiendo un prototipo siguiendo un prototipo,  */
+
+class Declaratoria{
+    constructor(var1,var2){
+        this.var1=var1;
+        this.var2=var2;
+    }
+
+    llamado(){
+
+        return this.var1 + this.var2;
+    }
+}
+
+const sum = new Declaratoria(1,2);
+console.log(sum.llamado())
+
+
+/* Metodo de la funcion especial
+#################################*/
+
+function Convension(var1,var2){
+    this.var1=var1;
+    this.var2=var2;
+}
+
+Convension.prototype.var=function(){
+    return this.var1 + this.var2 
+}
+
+const sum = new Convension(1,1);
+console.log(sum.var())
+
+/* CLASES DECLARATORIAS METODO DEL CONSTRUCTOR Y ESTÁTICO 
+#########################################################*
+
+Asi la clase tenga el metodo del cosntructor no se instancia la clase; es decir, las propiedades no lleban el prototipo
+del keyword this que es el encargado de instancia cada propiedad del objeto.*/
+
+
+
+class Fecha {
+    constructor(dia, mes, ano) {
+      this.dia = dia;
+      this.mes = mes;
+      this.ano = ano;
+    }
+  
+    static hoy() {
+      var fecha = new Date();
+      var dia = fecha.getDate();
+      var mes = fecha.getMonth() + 1;
+      var ano = fecha.getFullYear();
+      return new Fecha(dia, mes, ano);
+    }
+}
+  
+
+/* ################################################
+#######      C  A  L  L  B  A  C  K  S      ######
+################################################### */
+
+/* Firts class funstions () */
+
+
+/* pasa a las funciones como argumentos, */
+
+
+function Hola(usuario){
+    return "Hola " + usuario + " !";
+}
+
+function Adios (usuario){
+    return "adios " + usuario + " !";
+}
+
+function saludo(usuario, cb){
+    return cb(usuario);
+}
+
+Hola('Luis');
+Adios('Luis');
+saludo("Luis", Hola);
+
+/* modificar la función */
+
+console.log(saludo("Liz", function (a) { return a + " eres hermosa"}));
+
+
+/* CALBACKS EN ARREGLOS (ARRAYS) */
+/* ############################## */
+
+/* Otros tipo0s de claback son los relacioados a arreglos, los cuales agregan elemnetos (array.push)
+quitan elementos (array.pop)... 
+
+Estos son vistos como prototipos de los arreglos, por tanto tenemos varios prototipos ademas de 
+los anteriores; tenemos el prtotipo forEach*/
+
+
+
+/* Prototipo forEach
+##################### */
+
+/* forEcha ejecuata la funcion por cada elemento del array */
+
+var alumnos =['diego', 'edith', 'alejandro','duvan'];
+
+/* Sin colback */
+
+for (var i=0; i< alumnos.length; i++){
+    console.log(alumnos[i]);
+}
+/* Con colback */
+
+var alumnos =['diego', 'edith', 'alejandro','duvan'];
+
+ alumnos.forEach(function(elemento,indice){
+     console.log(elemento);
+});
+
+
+/* CALBACKS con MAPS 
+  #######################*/
+/* Crea un nuevo arreglo aplicada la función para la crración de cada elemento a partir del arreglo anterior */
+
+var num = [1,2,3,4];
+
+var numdouble = num.map(function(x){    /* 'map' mapea cada elemento del array y lo invoca con la nueva funcion */
+    return Math.pow(x,2);
+    
+})
+
+numdouble;
+
+
+/* con colbacks*/
+
+var nuevonum= [];
+for(var i=0; i<num.length; i++){
+    nuevonum.push(num[i].capitalize());
+}
+
+/* CALBAKS CON REDUCE 
+######################*/
+
+
+/* Reduce aplica una funcion reductora sobre cada elemnto del array devolviendo un único valor */
+
+var num = [1,2,3,4,5,6,7,8,9];
+
+/* sin calbacks */
+var suma = 0;
+for (var i=0; i< num.length; i=i+1){
+    suma = suma + num[i];
+}
+
+/* con calbacks */
+
+var sumaReduce = num.reduce(function(acumulador,elemnto){
+    return acumulador + elemnto
+},0)
+
+
+
+
--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,39 +1,73 @@
 // No cambies los nombres de las funciones.
 
+
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+
+  console.log(array[0])
+
+  
 }
+
+console.log(devolverPrimerElemento([1,2,3,4]))
+
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  
+  console.log(array[array.length-1]); 
 }
+
+console.log(devolverUltimoElemento([1,2,3,4]));
+
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  console.log(array.length);
 }
+
+console.log(obtenerLargoDelArray([1,2,3,4]));
 
 function incrementarPorUno(array) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  array.push(array.length + 1);
+
+  return array;
 }
+
+console.log(incrementarPorUno([1,2,3,4]));
+
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+
+  array.push(elemento)
+
+  console.log(array);
 }
+
+console.log(agregarItemAlFinalDelArray([0,1,2,3],4));
 
 function agregarItemAlComienzoDelArray(array, elemento) {
   // Añade el "elemento" al comienzo del array
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+
+  array.unshift(elemento);
+  console.log(array);
 }
+
+console.log(agregarItemAlComienzoDelArray([0,1,2,3],-1));
+
 
 function dePalabrasAFrase(palabras) {
   // "palabras" es un array de strings/cadenas
@@ -41,37 +75,75 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  
+  return palabras[3]+palabras[2]+palabras[5]+palabras[0]+palabras[1]+palabras[4]+palabras[6];
 }
+
+console.log(dePalabrasAFrase(['día ','maravilloso ', 'es ', 'Hoy ', 'para ', 'un ', 'sonrreir!! ']));
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  if (array.includes(elemento)){
+    return true;
+
+  }
+
+  return false;
+
+
 }
+
+console.log(arrayContiene([1,2,3,4],4));
+console.log(arrayContiene([1,2,3,4],5));
+
+
+
 
 function agregarNumeros(numeros) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  console.log( numeros.reduce(function(numeros,b){return numeros + b},0))
 }
+agregarNumeros([1,2,3,4])
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  let suma=0;
+  for (let  i=0;i<resultadosTest.length; i++){    
+    suma += resultadosTest[i];
+     
+  }
+  console.log(suma);
 }
+
+promedioResultadosTest([1,2,3,4])
+
+
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  console.log (Math.max(...numeros));
+  
 }
+
+numeroMasGrande([1,2,3])
+
+
+
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+
 }
 
 // No modificar nada debajo de esta línea
